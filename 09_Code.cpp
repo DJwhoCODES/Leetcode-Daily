a62da1783ab8b979e36c227ff15ae7bcd73b1b6e
DJwhoCODES <jaindevanshu824@gmail.com>
2025-03-05 16:39:26 +0530

    Day - 9

---
+++ 09_Code.cpp
@@ -0,0 +1,9 @@
+// 2579. Count Total Number of Colored Cells
+
+
+class Solution {
+    public:
+        long long coloredCells(int n) {
+            return 1 + 4LL * n * (n - 1) / 2; 
+        }
+    };
